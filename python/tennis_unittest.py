92e290372e2bfbeabb0ed360cdcbcdc6362d42be
Emily Bache <emily@bacheconsulting.com>
2013-07-24 07:20:34 +0200

    clear deprecation warning when running with python 3

--- python/tennis_unittest.py
+++ python/tennis_unittest.py
@@ -66,19 +66,19 @@ class TestTennis(unittest.TestCase):
         for testcase in test_cases:
             (p1Points, p2Points, score, p1Name, p2Name) = testcase
             game = play_game(TennisGame1, p1Points, p2Points, p1Name, p2Name)
-            self.assertEquals(score, game.score())
+            self.assertEqual(score, game.score())
 
     def test_Score_Game2(self):
         for testcase in test_cases:
             (p1Points, p2Points, score, p1Name, p2Name) = testcase
             game = play_game(TennisGame2, p1Points, p2Points, p1Name, p2Name)
-            self.assertEquals(score, game.score())
+            self.assertEqual(score, game.score())
 
     def test_Score_Game3(self):
         for testcase in test_cases:
             (p1Points, p2Points, score, p1Name, p2Name) = testcase
             game = play_game(TennisGame3, p1Points, p2Points, p1Name, p2Name)
-            self.assertEquals(score, game.score())
+            self.assertEqual(score, game.score())
  
 if __name__ == "__main__":
     unittest.main() 
