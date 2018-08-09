d7be7ed5db19e98f5d1e42d8e0b6d12c80b238da
Jerome Mottet <jerome.mottet@holidaycheck.com>
2018-08-09 13:56:14 +0200

    Replace loop by string concatenation

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -12,7 +12,7 @@ class TennisGame1 {
 
     getScore() {
         let score = '';
-        let tempScore = 0;
+
         if (this.m_score1 === this.m_score2) {           
             const equality = ['Love-All', 'Fifteen-All', 'Thirty-All', 'Deuce'];
             this.m_score1 > 3 ? score = equality[3] : score = equality[this.m_score1];
@@ -24,16 +24,9 @@ class TennisGame1 {
             minusResult >= 2 && (score = `Win for player1`);
             minusResult <= -2 && (score = `Win for player2`);
         } else {
-            for (let i = 1; i < 3; i++) {
-                if (i === 1) tempScore = this.m_score1;
-                else {
-                    score += "-";
-                    tempScore = this.m_score2;
-                }
-
-                const result = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-                score += result[tempScore]; 
-            }
+            const result = ['Love', 'Fifteen', 'Thirty', 'Forty'];
+            
+            score = `${result[this.m_score1]}-${result[this.m_score2]}`;
         }
         return score;
     }
