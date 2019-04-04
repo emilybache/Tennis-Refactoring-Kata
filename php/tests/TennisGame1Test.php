fd419b24695eea5202a7991a9821b5eb7498b132
Colin DeCarlo <colin@thedecarlos.ca>
2019-04-03 22:01:35 -0400

    Extract seedScores method

--- php/tests/TennisGame1Test.php
+++ php/tests/TennisGame1Test.php
@@ -31,15 +31,7 @@ class TennisGame1Test extends TestMaster
      */
     public function testScores($score1, $score2, $expectedResult)
     {
-        $highestScore = max($score1, $score2);
-        for ($i = 0; $i < $highestScore; $i++) {
-            if ($i < $score1) {
-                $this->game->wonPoint("player1");
-            }
-            if ($i < $score2) {
-                $this->game->wonPoint("player2");
-            }
-        }
+        $this->seedScores($score1, $score2);
         $this->assertEquals($expectedResult, $this->game->getScore());
     }
 }
