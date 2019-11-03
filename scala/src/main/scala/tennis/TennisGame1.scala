d3c03a74caafb72974e333048e8610980f72a811
Anass Daoudi <anass1995daoudi@gmail.com>
2019-11-03 22:34:44 +0100

    Refactor TennisGame1 class

--- scala/src/main/scala/tennis/TennisGame1.scala
+++ scala/src/main/scala/tennis/TennisGame1.scala
@@ -1,54 +1,69 @@
 package tennis
 
+final class Player (val name : String) {
+  var score: Int = 0
 
-class TennisGame1 (val player1Name : String, val player2Name : String) extends TennisGame {
-  var m_score1: Int = 0
-  var m_score2: Int = 0
+  def incrementScoreByOne () : Unit = {
+    score += 1
+  }
+}
 
-  def wonPoint(playerName : String) {
-          if (playerName == "player1")
-              m_score1 += 1
-          else
-              m_score2 += 1
+final class TennisGame1 (private val player1Name : String, private val player2Name : String) extends TennisGame {
+  private val player1 = new Player (player1Name)
+  private val player2 = new Player (player2Name) 
+  
+  def wonPoint(playerName : String) : Unit = {
+      if (playerName == player1.name) {
+          player1.incrementScoreByOne()
+      } else if (playerName == player2.name) {
+          player2.incrementScoreByOne()
+      } else {
+          throw new Exception ("Unknown player")
       }
+  }
 
-  def calculateScore() : String = {
-      var score : String = ""
-      var tempScore=0
-      if (m_score1==m_score2)
-      {
-        score = m_score1 match {
-              case 0 =>  "Love-All"
-              case 1 => "Fifteen-All"
-              case 2 => "Thirty-All"
-              case _ => "Deuce"
+  def calculateScore () : String = {
+    true match {
+      case x if x == areScoresEqual(player1.score, player2.score) => equalScoresResult(player1.score)
+      case x if x == isAdvantageResult(player1.score, player2.score) => advantageResult(player1, player2)
+      case _ => s"${runningScoreResult(player1.score)}-${runningScoreResult(player2.score)}"
+    }
+  }
 
-          }
+  private def areScoresEqual (player1Score : Int, player2Score : Int) : Boolean = {
+      player1Score == player2Score
+  }
+
+  private def isAdvantageResult (player1Score : Int, player2Score : Int) : Boolean = {
+      player1Score >= 4 || player2Score >= 4
+  }
+
+  private def advantageResult (player1 : Player, player2 : Player) : String = {
+      player1.score - player2.score match {
+          case 1 => s"Advantage ${player1.name}"
+          case -1 => s"Advantage ${player2.name}"
+          case x if x >= 2 => s"Win for ${player1.name}"
+          case _ => s"Win for ${player2.name}"
       }
-      else if (m_score1>=4 || m_score2>=4)
-      {
-          val minusResult = m_score1-m_score2
-          if (minusResult==1) score ="Advantage player1"
-          else if (minusResult == -1) score ="Advantage player2"
-          else if (minusResult>=2) score = "Win for player1"
-          else score ="Win for player2"
-      }
-      else
-      {
-          for ( i<- 1 until 3 by 1)
-          {
-              if (i==1) tempScore = m_score1
-              else { score+="-"; tempScore = m_score2;}
-              val tempScore2 = tempScore match {
-                  case 0 => "Love"
-                  case 1 => "Fifteen"
-                  case 2 => "Thirty"
-                  case 3 => "Forty"
-              }
-            score += tempScore2
-          }
-      }
-    return score
+  }
+
+  private def equalScoresResult (score : Int) : String = {
+    score match {
+        case 0 =>  "Love-All"
+        case 1 => "Fifteen-All"
+        case 2 => "Thirty-All"
+        case _ => "Deuce"
+
+    }
+  }
+
+  private def runningScoreResult (score : Int) : String = {
+    score match {
+        case 0 => "Love"
+        case 1 => "Fifteen"
+        case 2 => "Thirty"
+        case 3 => "Forty"
+    }
   }
 
 }
