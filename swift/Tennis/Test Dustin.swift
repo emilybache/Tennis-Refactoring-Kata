5cc99cccfbb0defec7a2904a38a8aa5a2e9a4912
dthosten-ford <54145242+dthosten-ford@users.noreply.github.com>
2020-08-31 10:28:58 -0500

    test dustin.swift

---
+++ swift/Tennis/Test Dustin.swift
@@ -0,0 +1,34 @@
+//
+//  Test Raja.swift
+//  TennisTests
+//
+//  Created by Poreddy, Raja on 8/31/20.
+//
+
+import Foundation
+
+
+
+class test {
+    
+    func testThis() {
+        
+    }
+    
+    func testThat() {
+        
+        
+    }
+}
+
+protocol Testing {
+    func hereYouGo()
+}
+
+class newClass: Testing {
+    func hereYouGo() {
+        print("not going good??")
+        
+        print("test some stuff here")
+    }
+}
