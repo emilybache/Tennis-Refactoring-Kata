793f2343d8c123ae71560f5fa08f755204124d2c
Corentin Leffy <corentin.leffy@gmail.com>
2020-11-30 20:51:25 +0100

    refactor: return directly instead of using a tmp variable

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -18,21 +18,21 @@ class TennisGame1(
         var tempScore = 0
         if (scorePlayer1 == scorePlayer2) {
             when (scorePlayer1) {
-                0 -> score = "Love-All"
-                1 -> score = "Fifteen-All"
-                2 -> score = "Thirty-All"
-                else -> score = "Deuce"
+                0 -> return "Love-All"
+                1 -> return "Fifteen-All"
+                2 -> return "Thirty-All"
+                else -> return "Deuce"
             }
         } else if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
             val minusResult = scorePlayer1 - scorePlayer2
             if (minusResult == 1)
-                score = "Advantage player1"
+                return "Advantage player1"
             else if (minusResult == -1)
-                score = "Advantage player2"
+                return "Advantage player2"
             else if (minusResult >= 2)
-                score = "Win for player1"
+                return "Win for player1"
             else
-                score = "Win for player2"
+                return "Win for player2"
         } else {
             for (i in 1..2) {
                 if (i == 1)
