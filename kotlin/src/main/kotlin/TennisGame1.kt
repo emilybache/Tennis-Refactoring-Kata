3ef3ec627af54b6beed9f154b55d0db5c6a39049
Corentin Leffy <corentin.leffy@gmail.com>
2020-12-03 20:06:30 +0100

    refactor: extract default behaviour inside a class

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -23,7 +23,7 @@ class TennisGame1(
             player2.hasAdvantageOver(player1) -> AdvantageScore(player2).toString()
             player1.hasWonAgainst(player2) -> WinScore(player1).toString()
             player2.hasWonAgainst(player1) -> WinScore(player2).toString()
-            else -> "${scoreByPoints[player1.points]}-${scoreByPoints[player2.points]}"
+            else -> NormalScore(player1, player2).toString()
         }
     }
 
@@ -34,6 +34,23 @@ class TennisGame1(
 
 abstract class Score {
     abstract override fun toString(): String
+}
+
+class NormalScore(
+    private val player1: Player,
+    private val player2: Player
+) : Score() {
+
+    private val scoreByPoints = mapOf(
+        0 to "Love",
+        1 to "Fifteen",
+        2 to "Thirty",
+        3 to "Forty"
+    )
+
+    override fun toString(): String =
+        "${scoreByPoints[player1.points]}-${scoreByPoints[player2.points]}"
+
 }
 
 class WinScore(
