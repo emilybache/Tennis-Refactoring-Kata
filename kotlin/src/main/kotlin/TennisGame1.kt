b342d2a5d64b739b584062b3b3351642ca7823a7
Corentin Leffy <corentin.leffy@gmail.com>
2020-12-03 19:48:34 +0100

    refactor: extract advantage behaviour inside a class

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -19,8 +19,8 @@ class TennisGame1(
     override fun getScore(): String {
         return when {
             player1.isInTieWith(player2) -> TieScore(player1).toString()
-            player1.hasAdvantageOver(player2) -> "Advantage ${player1.name}"
-            player2.hasAdvantageOver(player1) -> "Advantage ${player2.name}"
+            player1.hasAdvantageOver(player2) -> AdvantageScore(player1).toString()
+            player2.hasAdvantageOver(player1) -> AdvantageScore(player2).toString()
             player1.hasWonAgainst(player2) -> "Win for ${player1.name}"
             player2.hasWonAgainst(player1) -> "Win for ${player2.name}"
             else -> "${scoreByPoints[player1.points]}-${scoreByPoints[player2.points]}"
@@ -34,6 +34,12 @@ class TennisGame1(
 
 abstract class Score {
     abstract override fun toString(): String
+}
+
+class AdvantageScore(
+    private val player: Player
+) : Score() {
+    override fun toString(): String = "Advantage ${player.name}"
 }
 
 class TieScore(
