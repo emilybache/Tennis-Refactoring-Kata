067ca1350109c249a41009b8ce396d6dc4fd855d
Corentin Leffy <corentin.leffy@gmail.com>
2020-12-03 19:46:41 +0100

    refactor: extract tie behaviour inside a class

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,15 +1,15 @@
 class TennisGame1(
-        player1Name: String,
-        player2Name: String
+    player1Name: String,
+    player2Name: String
 ) : TennisGame {
     private val player1 = Player(player1Name)
     private val player2 = Player(player2Name)
 
     private val scoreByPoints = mapOf(
-            0 to "Love",
-            1 to "Fifteen",
-            2 to "Thirty",
-            3 to "Forty"
+        0 to "Love",
+        1 to "Fifteen",
+        2 to "Thirty",
+        3 to "Forty"
     )
 
     override fun wonPoint(playerName: String) {
@@ -18,7 +18,7 @@ class TennisGame1(
 
     override fun getScore(): String {
         return when {
-            player1.isInTieWith(player2) -> equalScore()
+            player1.isInTieWith(player2) -> TieScore(player1).toString()
             player1.hasAdvantageOver(player2) -> "Advantage ${player1.name}"
             player2.hasAdvantageOver(player1) -> "Advantage ${player2.name}"
             player1.hasWonAgainst(player2) -> "Win for ${player1.name}"
@@ -28,11 +28,19 @@ class TennisGame1(
     }
 
     private fun playerWithName(name: String): Player =
-            player1.takeIf { player -> player.isCalled(name) }
-                    ?: player2
+        player1.takeIf { player -> player.isCalled(name) }
+            ?: player2
+}
 
-    private fun equalScore(): String {
-        return when (player1.points) {
+abstract class Score {
+    abstract override fun toString(): String
+}
+
+class TieScore(
+    private val player: Player
+) : Score() {
+    override fun toString(): String {
+        return when (player.points) {
             0 -> "Love-All"
             1 -> "Fifteen-All"
             2 -> "Thirty-All"
