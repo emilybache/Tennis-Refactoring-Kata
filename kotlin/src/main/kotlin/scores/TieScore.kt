8ce6ab05a2b30198c401ce286873a5b8badfac1c
Corentin Leffy <corentin.leffy@gmail.com>
2020-12-03 20:31:42 +0100

    refactor: use a map for tie scores

--- kotlin/src/main/kotlin/scores/TieScore.kt
+++ kotlin/src/main/kotlin/scores/TieScore.kt
@@ -4,13 +4,14 @@ import Player
 import Score
 
 class TieScore(player1: Player, player2: Player) : Score(player1, player2) {
-    override fun toString(): String =
-        when (player1.points) {
-            0 -> "Love-All"
-            1 -> "Fifteen-All"
-            2 -> "Thirty-All"
-            else -> "Deuce"
-        }
+
+    private val scores = mapOf(
+        0 to "Love-All",
+        1 to "Fifteen-All",
+        2 to "Thirty-All"
+    ).withDefault { "Deuce" }
+
+    override fun toString(): String = scores.getValue(player1.points)
 
     override fun isApplicable(): Boolean = player1.isInTieWith(player2)
 }
