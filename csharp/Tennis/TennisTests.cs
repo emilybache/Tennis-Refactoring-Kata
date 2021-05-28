97928a1c705bbe6386e9d673fe3717f1466005af
emily <emily@bacheconsulting.com>
2021-05-28 15:14:41 +0200

    Convert tests from NUnit to XUnit

--- csharp/Tennis/TennisTests.cs
+++ csharp/Tennis/TennisTests.cs
@@ -1,125 +1,92 @@
 using System;
-using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
 
 namespace Tennis
 {
-    [TestFixture( 0,  0, "Love-All")]
-    [TestFixture( 1,  1, "Fifteen-All")]
-    [TestFixture( 2,  2, "Thirty-All")]
-    [TestFixture( 3,  3, "Deuce")]
-    [TestFixture( 4,  4, "Deuce")]
-    [TestFixture( 1,  0, "Fifteen-Love")]
-    [TestFixture( 0,  1, "Love-Fifteen")]
-    [TestFixture( 2,  0, "Thirty-Love")]
-    [TestFixture( 0,  2, "Love-Thirty")]
-    [TestFixture( 3,  0, "Forty-Love")]
-    [TestFixture( 0,  3, "Love-Forty")]
-    [TestFixture( 4,  0, "Win for player1")]
-    [TestFixture( 0,  4, "Win for player2")]
-    [TestFixture( 2,  1, "Thirty-Fifteen")]
-    [TestFixture( 1,  2, "Fifteen-Thirty")]
-    [TestFixture( 3,  1, "Forty-Fifteen")]
-    [TestFixture( 1,  3, "Fifteen-Forty")]
-    [TestFixture( 4,  1, "Win for player1")]
-    [TestFixture( 1,  4, "Win for player2")]
-    [TestFixture( 3,  2, "Forty-Thirty")]
-    [TestFixture( 2,  3, "Thirty-Forty")]
-    [TestFixture( 4,  2, "Win for player1")]
-    [TestFixture( 2,  4, "Win for player2")]
-    [TestFixture( 4,  3, "Advantage player1")]
-    [TestFixture( 3,  4, "Advantage player2")]
-    [TestFixture( 5,  4, "Advantage player1")]
-    [TestFixture( 4,  5, "Advantage player2")]
-    [TestFixture(15, 14, "Advantage player1")]
-    [TestFixture(14, 15, "Advantage player2")]
-    [TestFixture( 6,  4, "Win for player1")]
-    [TestFixture( 4,  6, "Win for player2")]
-    [TestFixture(16, 14, "Win for player1")]
-    [TestFixture(14, 16, "Win for player2")]
+    public class TestDataGenerator : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new List<object[]>
+        {
+            new object[] {0, 0, "Love-All"},
+            new object[] {1, 1, "Fifteen-All"},
+            new object[] {2, 2, "Thirty-All"},
+            new object[] {3, 3, "Deuce"},
+            new object[] {4, 4, "Deuce"},
+            new object[] {1, 0, "Fifteen-Love"},
+            new object[] {0, 1, "Love-Fifteen"},
+            new object[] {2, 0, "Thirty-Love"},
+            new object[] {0, 2, "Love-Thirty"},
+            new object[] {3, 0, "Forty-Love"},
+            new object[] {0, 3, "Love-Forty"},
+            new object[] {4, 0, "Win for player1"},
+            new object[] {0, 4, "Win for player2"},
+            new object[] {2, 1, "Thirty-Fifteen"},
+            new object[] {1, 2, "Fifteen-Thirty"},
+            new object[] {3, 1, "Forty-Fifteen"},
+            new object[] {1, 3, "Fifteen-Forty"},
+            new object[] {4, 1, "Win for player1"},
+            new object[] {1, 4, "Win for player2"},
+            new object[] {3, 2, "Forty-Thirty"},
+            new object[] {2, 3, "Thirty-Forty"},
+            new object[] {4, 2, "Win for player1"},
+            new object[] {2, 4, "Win for player2"},
+            new object[] {4, 3, "Advantage player1"},
+            new object[] {3, 4, "Advantage player2"},
+            new object[] {5, 4, "Advantage player1"},
+            new object[] {4, 5, "Advantage player2"},
+            new object[] {15, 14, "Advantage player1"},
+            new object[] {14, 15, "Advantage player2"},
+            new object[] {6, 4, "Win for player1"},
+            new object[] {4, 6, "Win for player2"},
+            new object[] {16, 14, "Win for player1"},
+            new object[] {14, 16, "Win for player2"},
+        };
+
+        public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
     public class TennisTests
     {
-        private readonly int player1Score;
-        private readonly int player2Score;
-        private readonly string expectedScore;
-
-        public TennisTests(int player1Score, int player2Score, string expectedScore)
-        {
-            this.player1Score = player1Score;
-            this.player2Score = player2Score;
-            this.expectedScore = expectedScore;
-        }
-
-        [Test]
-        public void CheckTennisGame1()
+        [Theory]
+        [ClassData(typeof(TestDataGenerator))]
+        public void Tennis1Test(int p1, int p2, string expected)
         {
             var game = new TennisGame1("player1", "player2");
-            CheckAllScores(game);
+            CheckAllScores(game, p1, p2, expected);
         }
 
-        [Test]
-        public void CheckTennisGame2()
+        [Theory]
+        [ClassData(typeof(TestDataGenerator))]
+        public void Tennis2Test(int p1, int p2, string expected)
         {
             var game = new TennisGame2("player1", "player2");
-            CheckAllScores(game);
+            CheckAllScores(game, p1, p2, expected);
         }
 
-        [Test]
-        public void CheckTennisGame3()
+        [Theory]
+        [ClassData(typeof(TestDataGenerator))]
+        public void Tennis3Test(int p1, int p2, string expected)
         {
             var game = new TennisGame3("player1", "player2");
-            CheckAllScores(game);
+            CheckAllScores(game, p1, p2, expected);
         }
 
-        private void CheckAllScores(ITennisGame game)
+        private void CheckAllScores(ITennisGame game, int player1Score, int player2Score, string expectedScore)
         {
-            var highestScore = Math.Max(this.player1Score, this.player2Score);
+            var highestScore = Math.Max(player1Score, player2Score);
             for (var i = 0; i < highestScore; i++)
             {
-                if (i < this.player1Score)
+                if (i < player1Score)
                     game.WonPoint("player1");
-                if (i < this.player2Score)
+                if (i < player2Score)
                     game.WonPoint("player2");
             }
-            Assert.AreEqual(this.expectedScore, game.GetScore());
-        }
 
-    }
-
-    [TestFixture]
-    public class ExampleGameTennisTest
-    {
-        [Test]
-        public void CheckGame1()
-        {
-            var game = new TennisGame1("player1", "player2");
-            RealisticTennisGame(game);
-        }
-
-        [Test]
-        public void CheckGame2()
-        {
-            var game = new TennisGame2("player1", "player2");
-            RealisticTennisGame(game);
-        }
-
-        [Test]
-        public void CheckGame3()
-        {
-            var game = new TennisGame3("player1", "player2");
-            RealisticTennisGame(game);
-        }
-
-        private void RealisticTennisGame(ITennisGame game)
-        {
-            string[] points = { "player1", "player1", "player2", "player2", "player1", "player1" };
-            string[] expectedScores = { "Fifteen-Love", "Thirty-Love", "Thirty-Fifteen", "Thirty-All", "Forty-Thirty", "Win for player1" };
-            for (var i = 0; i < 6; i++)
-            {
-                game.WonPoint(points[i]);
-                Assert.AreEqual(expectedScores[i], game.GetScore());
-            }
+            Assert.Equal(expectedScore, game.GetScore());
         }
     }
-
 }
-
