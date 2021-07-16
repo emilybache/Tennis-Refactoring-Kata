1fc987d18c26daee12685e35c7a7ca25a35887cc
Maurici Abad <maurici.abad@glovoapp.com>
2021-07-16 17:19:15 +0200

    Make player class

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -1,68 +1,79 @@
 import { TennisGame } from './TennisGame'
 
+class Player {
+  constructor(public name: string, public score: number = 0) {}
+}
+
 export class TennisGame1 implements TennisGame {
-  private m_score1: number = 0
-  private m_score2: number = 0
-  private player1Name: string
-  private player2Name: string
+  private player1: Player
+  private player2: Player
 
   constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name
-    this.player2Name = player2Name
+    this.player1 = new Player(player1Name)
+    this.player2 = new Player(player2Name)
   }
 
   wonPoint(playerName: string): void {
-    if (playerName === 'player1') this.m_score1 += 1
-    else this.m_score2 += 1
+    if (playerName === 'player1') this.player1.score += 1
+    else this.player2.score += 1
   }
 
   getScore(): string {
     if (this.isDraw()) {
-      return this.scoreWhenDraw();
+      return this.scoreWhenDraw()
     }
     if (this.oneOfTheTwoScoredForty()) {
       return this.calculateWinnerOrAdvantage()
     }
 
-    return  this.nonWinningScore();
+    return this.nonWinningScore()
   }
 
   private scoreWhenDraw() {
-    if (this.m_score1 < 3) {
-      return this.getScoreName(this.m_score1) + '-All'
+    if (this.player1.score < 3) {
+      return this.getScoreName(this.player1.score) + '-All'
     } else {
       return 'Deuce'
     }
   }
 
   private oneOfTheTwoScoredForty() {
-    return this.m_score1 >= 4 || this.m_score2 >= 4;
+    return this.player1.score >= 4 || this.player2.score >= 4
   }
 
   private isDraw() {
-    return this.m_score1 === this.m_score2;
+    return this.player1.score === this.player2.score
   }
 
   private nonWinningScore() {
-    return this.getScoreName(this.m_score1) + '-' + this.getScoreName(this.m_score2);
+    return (
+      this.getScoreName(this.player1.score) +
+      '-' +
+      this.getScoreName(this.player2.score)
+    )
   }
 
   private getScoreName(score: number) {
     switch (score) {
-      case 0: return  'Love'
-      case 1: return  'Fifteen'
-      case 2: return  'Thirty'
-      case 3: return  'Forty'
-      default: return ''
+      case 0:
+        return 'Love'
+      case 1:
+        return 'Fifteen'
+      case 2:
+        return 'Thirty'
+      case 3:
+        return 'Forty'
+      default:
+        return ''
     }
   }
 
   private calculateWinnerOrAdvantage(): string {
-    const scoreDifference = Math.abs(this.m_score1 - this.m_score2)
+    const scoreDifference = Math.abs(this.player1.score - this.player2.score)
     const winningPlayer =
-      this.m_score1 > this.m_score2 ? this.player1Name : this.player2Name
+      this.player1.score > this.player2.score ? this.player1 : this.player2
 
-    if (scoreDifference === 1) return 'Advantage ' + winningPlayer
-    if (scoreDifference >= 2) return 'Win for ' + winningPlayer
+    if (scoreDifference === 1) return 'Advantage ' + winningPlayer.name
+    if (scoreDifference >= 2) return 'Win for ' + winningPlayer.name
   }
 }
