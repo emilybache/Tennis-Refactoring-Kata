7e50256335ebf97d134c5c2b23eede670acd97e5
HUNTER\mobpilot2.user <mobpilot2user@hunterindustries.com>
2021-08-03 15:38:46 -0700

    t - improve test coverage of label text for overall score for tennis game 3

--- angular/src/app/tennis-game3/tennis-game3.component.spec.ts
+++ angular/src/app/tennis-game3/tennis-game3.component.spec.ts
@@ -1,7 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { TennisGame3Component } from './tennis-game3.component';
+import {TennisGame3Component} from './tennis-game3.component';
 import {expectedScores} from '../../test/testHelper';
+import {By} from '@angular/platform-browser';
 
 describe('TennisGame3Component', () => {
   let component: TennisGame3Component;
@@ -29,13 +30,13 @@ describe('TennisGame3Component', () => {
 
   expectedScores.forEach(([player1Score, player2Score, expectedScore]) => {
     it(`should score '${expectedScore}' when player 1 has '${player1Score}' and player 2 has '${player2Score}'`, () => {
-      expect(component.overallScore).toBe('');
+      verifyLabelText('#overall-score', '');
 
       setInputValue('#player-one-score', player1Score);
       setInputValue('#player-two-score', player2Score);
       selectElement('#get-score-button');
 
-      expect(component.overallScore).toBe(expectedScore as string);
+      verifyLabelText('#overall-score', expectedScore);
     });
 
     function setInputValue(inputSelector: string, newValue: number | string) {
@@ -44,9 +45,15 @@ describe('TennisGame3Component', () => {
       player1ScoreInput.dispatchEvent(new Event('input'));
     }
 
-    function selectElement(selector: string) {
-      const getScoreButton = element.querySelector(selector);
+    function selectElement(elementSelector: string) {
+      const getScoreButton = element.querySelector(elementSelector);
       getScoreButton.click();
+      fixture.detectChanges();
+    }
+
+    function verifyLabelText(labelSelector: string, expectedText: number | string) {
+      const overallScore = fixture.debugElement.query(By.css(labelSelector));
+      expect(overallScore.nativeElement.outerText).toBe(expectedText);
     }
   });
 });
