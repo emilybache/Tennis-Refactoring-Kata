08247a1951a94e917bf68f4eb283f7669b89b95b
HUNTER\mobpilot2.user <mobpilot2user@hunterindustries.com>
2021-08-14 02:24:17 -0700

    F - updated look and feel for tennis game 3

--- angular/src/app/tennis-game3/tennis-game3.component.ts
+++ angular/src/app/tennis-game3/tennis-game3.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {TennisGame} from '../TennisGame';
 import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-tennis-game3',
   templateUrl: './tennis-game3.component.html',
-  styleUrls: ['./tennis-game3.component.scss']
+  styleUrls: ['./tennis-game3.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TennisGame3Component implements OnInit, TennisGame {
   private p2 = 0;
