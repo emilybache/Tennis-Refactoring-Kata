286220b12756978eb7ac73699929e44139b85324
HUNTER\mobpilot2.user <mobpilot2user@hunterindustries.com>
2021-08-22 07:01:28 -0700

    t - extract consts

--- angular/src/test/expectedResults.ts
+++ angular/src/test/expectedResults.ts
@@ -9,72 +9,93 @@ export const expectedText = {
   errorMessage: 'Something has gone wrong, please try again'
 };
 
+const loveAll = 'Love-All';
+const fifteenAll = 'Fifteen-All';
+const thirtyAll = 'Thirty-All';
+const deuce = 'Deuce';
+const fifteenLove = 'Fifteen-Love';
+const loveFifteen = 'Love-Fifteen';
+const thirtyLove = 'Thirty-Love';
+const loveThirty = 'Love-Thirty';
+const fortyLove = 'Forty-Love';
+const loveForty = 'Love-Forty';
+const winForPlayer1 = 'Win for player1';
+const winForPlayer2 = 'Win for player2';
+const thirtyFifteen = 'Thirty-Fifteen';
+const fifteenThirty = 'Fifteen-Thirty';
+const fortyFifteen = 'Forty-Fifteen';
+const fifteenForty = 'Fifteen-Forty';
+const fortyThirty = 'Forty-Thirty';
+const thirtyForty = 'Thirty-Forty';
+const advantagePlayer1 = 'Advantage player1';
+const advantagePlayer2 = 'Advantage player2';
+const invalidScore = 'Invalid Score';
+
 export const expectedTennisScores = [
-    [0.0, 0, 'Love-All'],
-    [0, 0, 'Love-All'],
-    [1, 1, 'Fifteen-All'],
-    [1, 1.0, 'Fifteen-All'],
-    [2, 2, 'Thirty-All'],
-    [3, 3, 'Deuce'],
-    [4, 4, 'Deuce'],
+    [0.0, 0, loveAll],
+    [0, 0, loveAll],
+    [1, 1, fifteenAll],
+    [1, 1.0, fifteenAll],
+    [2, 2, thirtyAll],
+    [3, 3, deuce],
+    [4, 4, deuce],
 
-    [1, 0, 'Fifteen-Love'],
-    [0, 1, 'Love-Fifteen'],
-    [2, 0, 'Thirty-Love'],
-    [0, 2, 'Love-Thirty'],
-    [3, 0, 'Forty-Love'],
-    [0, 3, 'Love-Forty'],
-    [4, 0, 'Win for player1'],
-    [0, 4, 'Win for player2'],
+    [1, 0, fifteenLove],
+    [0, 1, loveFifteen],
+    [2, 0, thirtyLove],
+    [0, 2, loveThirty],
+    [3, 0, fortyLove],
+    [0, 3, loveForty],
+    [4, 0, winForPlayer1],
+    [0, 4, winForPlayer2],
 
-    [2, 1, 'Thirty-Fifteen'],
-    [1, 2, 'Fifteen-Thirty'],
-    [3, 1, 'Forty-Fifteen'],
-    [1, 3, 'Fifteen-Forty'],
-    [4, 1, 'Win for player1'],
-    [1, 4, 'Win for player2'],
+    [2, 1, thirtyFifteen],
+    [1, 2, fifteenThirty],
+    [3, 1, fortyFifteen],
+    [1, 3, fifteenForty],
+    [4, 1, winForPlayer1],
+    [1, 4, winForPlayer2],
 
-    [3, 2, 'Forty-Thirty'],
-    [2, 3, 'Thirty-Forty'],
-    [4, 2, 'Win for player1'],
-    [2, 4, 'Win for player2'],
+    [3, 2, fortyThirty],
+    [2, 3, thirtyForty],
+    [4, 2, winForPlayer1],
+    [2, 4, winForPlayer2],
 
-    [4, 3, 'Advantage player1'],
-    [3, 4, 'Advantage player2'],
-    [5, 4, 'Advantage player1'],
-    [4, 5, 'Advantage player2'],
-    [15, 14, 'Advantage player1'],
-    [14, 15, 'Advantage player2'],
+    [4, 3, advantagePlayer1],
+    [3, 4, advantagePlayer2],
+    [5, 4, advantagePlayer1],
+    [4, 5, advantagePlayer2],
+    [15, 14, advantagePlayer1],
+    [14, 15, advantagePlayer2],
 
-    [6, 4, 'Win for player1'],
-    [4, 6, 'Win for player2'],
-    [16, 14, 'Win for player1'],
-    [14, 16, 'Win for player2'],
+    [6, 4, winForPlayer1],
+    [4, 6, winForPlayer2],
+    [16, 14, winForPlayer1],
+    [14, 16, winForPlayer2],
 
-    [-1, 0, 'Invalid Score'],
-    [-99, 0, 'Invalid Score'],
-    [0, -1, 'Invalid Score'],
-    [0, -99, 'Invalid Score'],
-    ['not a number', 0, 'Invalid Score'],
-    ['!@#$%^&*()_', 0, 'Invalid Score'],
-    ['1x', 0, 'Invalid Score'],
-    [1.6, 0, 'Invalid Score'],
-    [0.6, 0, 'Invalid Score'],
-    [1 / 4, 0, 'Invalid Score'],
-    [0, 'not a number', 'Invalid Score'],
-    [0, '!@#$%^&*()_', 'Invalid Score'],
-    [0, '1x', 'Invalid Score'],
-    ['1x', 0, 'Invalid Score'],
-    [0, 1.6, 'Invalid Score'],
-    [0, 0.6, 'Invalid Score'],
-    [0, 1 / 4, 'Invalid Score'],
-    // todo: catch exceptions thrown from new inline service called Zanzibar
-    // todo: extract magic strings above for better names
+    [-1, 0, invalidScore],
+    [-99, 0, invalidScore],
+    [0, -1, invalidScore],
+    [0, -99, invalidScore],
+
+    ['not a number', 0, invalidScore],
+    ['!@#$%^&*()_', 0, invalidScore],
+    ['1x', 0, invalidScore],
+    [0, 'not a number', invalidScore],
+    [0, '!@#$%^&*()_', invalidScore],
+    [0, '1x', invalidScore],
+
+    [1.6, 0, invalidScore],
+    [0.6, 0, invalidScore],
+    [1 / 4, 0, invalidScore],
+    [0, 1.6, invalidScore],
+    [0, 0.6, invalidScore],
+    [0, 1 / 4, invalidScore],
     // todo: extract blocks of the array into named sub arrays
     // todo: manual test to find more ZOMBIES
     // todo: any other ZOMBIES?
     // todo: test drive add html attributes to prevent bad input e.g. negative, non-int, etc.
-    // todo: separate out error message from score and show it on the gui
+    // todo: separate out error message from score and show it on the gui (one error message label or one per input?)
     // todo: expect different error messages for different kinds of issues
     // todo: any other ZOMBIES?
     // todo: make product error handling code more dirty (each component different)
