b9ce19aa0e313b3d288f2b3379f98b797167e2e3
JonathanRaso <57390276+JonathanRaso@users.noreply.github.com>
2022-04-13 11:15:19 +0200

    Update tennis_test.py
    Duplicate tests removed

--- tennis_test.py
+++ tennis_test.py
@@ -13,17 +13,3 @@ class TestTennis:
     def test_get_score_game1(self, p1Points, p2Points, score, p1Name, p2Name):
         game = play_game(TennisGame, p1Points, p2Points, p1Name, p2Name)
         assert score == game.score()
-
-    @pytest.mark.parametrize(
-        "p1Points p2Points score p1Name p2Name".split(), test_cases
-    )
-    def test_get_score_game2(self, p1Points, p2Points, score, p1Name, p2Name):
-        game = play_game(TennisGame, p1Points, p2Points, p1Name, p2Name)
-        assert score == game.score()
-
-    @pytest.mark.parametrize(
-        "p1Points p2Points score p1Name p2Name".split(), test_cases
-    )
-    def test_get_score_game3(self, p1Points, p2Points, score, p1Name, p2Name):
-        game = play_game(TennisGame, p1Points, p2Points, p1Name, p2Name)
-        assert score == game.score()
