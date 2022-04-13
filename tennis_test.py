e33ab626a5f96f933295b310546be8a6bbb193c3
JonathanRaso <57390276+JonathanRaso@users.noreply.github.com>
2022-04-13 10:46:57 +0200

    Update tennis_test.py
    Class name changed (TennisGame1 => TennisGame)

--- tennis_test.py
+++ tennis_test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from tennis import TennisGame1
+from tennis import TennisGame
 
 from tennis_unittest import test_cases, play_game
 
@@ -11,19 +11,19 @@ class TestTennis:
         "p1Points p2Points score p1Name p2Name".split(), test_cases
     )
     def test_get_score_game1(self, p1Points, p2Points, score, p1Name, p2Name):
-        game = play_game(TennisGame1, p1Points, p2Points, p1Name, p2Name)
+        game = play_game(TennisGame, p1Points, p2Points, p1Name, p2Name)
         assert score == game.score()
 
     @pytest.mark.parametrize(
         "p1Points p2Points score p1Name p2Name".split(), test_cases
     )
     def test_get_score_game2(self, p1Points, p2Points, score, p1Name, p2Name):
-        game = play_game(TennisGame1, p1Points, p2Points, p1Name, p2Name)
+        game = play_game(TennisGame, p1Points, p2Points, p1Name, p2Name)
         assert score == game.score()
 
     @pytest.mark.parametrize(
         "p1Points p2Points score p1Name p2Name".split(), test_cases
     )
     def test_get_score_game3(self, p1Points, p2Points, score, p1Name, p2Name):
-        game = play_game(TennisGame1, p1Points, p2Points, p1Name, p2Name)
+        game = play_game(TennisGame, p1Points, p2Points, p1Name, p2Name)
         assert score == game.score()
