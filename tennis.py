123f201b9c8443f0c2ecd07ae0cb5c3b2ff47cf8
Melody Migliaccio <welodya@gmail.com>
2022-04-14 14:52:38 +0200

    modif

--- tennis.py
+++ tennis.py
@@ -15,14 +15,14 @@ class TennisGame:
         self.player2 = Player(player2Name)
 
     def won_point(self, playerName):
+        if playerName != self.player1.name and self.playerName != self.player2.name:
+            raiseExceptions('Player not existing!')
+        if (self.player1.points >= 4 or self.player2.points >= 4) and abs(self.player1.points - self.player2.points) >= 2:
+            raiseExceptions('Game is over !')
         if playerName == self.player1.name:
             self.player1.points += 1
         else:
             self.player2.points += 1
-        if playerName != self.player1.name or self.player2.name:
-            raiseExceptions('Player not existing!')
-        if (self.player1.points >= 4 or self.player2.points >= 4) and abs(self.player1.points - self.player2.points) >= 2:
-            raiseExceptions('Game is over !')
 
     def score(self):
         if (self.player1.points < 4 and self.player2.points < 4) and (
