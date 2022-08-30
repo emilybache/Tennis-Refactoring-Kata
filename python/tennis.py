71f3772cdff023f3ddeefdd47120e4d6e782a371
Sulidi Maimaitiming <sulidi.maimaitiming.ext@qonto.com>
2022-08-30 21:32:59 +0200

    Comparison is transitive

--- python/tennis.py
+++ python/tennis.py
@@ -164,7 +164,7 @@ class TennisGame3:
             p = ["Love", "Fifteen", "Thirty", "Forty"]
             s = p[self.p1]
             return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
-        if (self.p1 >= 4 or self.p2 >= 4) and self.p1 == self.p2:
+        if self.p1 == self.p2 >= 4:
             return "Deuce"
 
         if (self.p1 >= 4 or self.p2 >= 4) and self.p1 != self.p2:
