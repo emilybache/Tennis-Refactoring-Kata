a5fabd024a816aed0672936916e8e2ec40c5797f
Sulidi Maimaitiming <sulidi.maimaitiming.ext@qonto.com>
2022-08-30 21:26:26 +0200

    Break or appart

--- python/tennis.py
+++ python/tennis.py
@@ -164,7 +164,19 @@ class TennisGame3:
             p = ["Love", "Fifteen", "Thirty", "Forty"]
             s = p[self.p1]
             return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
-        if (self.p1 >= 4 or self.p2 >= 4 or self.p1 == self.p2 == 3):
+        if self.p1 >= 4:
+            if (self.p1 == self.p2):
+                return "Deuce"
+            s = self.p1N if self.p1 > self.p2 else self.p2N
+            return "Advantage " + s if ((self.p1-self.p2)*(self.p1-self.p2) == 1) else "Win for " + s
+
+        if self.p2 >= 4:
+            if (self.p1 == self.p2):
+                return "Deuce"
+            s = self.p1N if self.p1 > self.p2 else self.p2N
+            return "Advantage " + s if ((self.p1-self.p2)*(self.p1-self.p2) == 1) else "Win for " + s
+
+        if self.p1 == self.p2 == 3:
             if (self.p1 == self.p2):
                 return "Deuce"
             s = self.p1N if self.p1 > self.p2 else self.p2N
