99264da88da8120085811b860cbd5e2c880a87c2
Sulidi Maimaitiming <sulidi.maimaitiming.ext@qonto.com>
2022-08-30 21:29:43 +0200

    Distribute condition

--- python/tennis.py
+++ python/tennis.py
@@ -164,12 +164,12 @@ class TennisGame3:
             p = ["Love", "Fifteen", "Thirty", "Forty"]
             s = p[self.p1]
             return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
-        if (self.p1 >= 4 or self.p2 >= 4):
-            if (self.p1 == self.p2):
-                return "Deuce"
-            else:
-                s = self.p1N if self.p1 > self.p2 else self.p2N
-                return "Advantage " + s if ((self.p1-self.p2)*(self.p1-self.p2) == 1) else "Win for " + s
+        if (self.p1 >= 4 or self.p2 >= 4) and self.p1 == self.p2:
+            return "Deuce"
+
+        if (self.p1 >= 4 or self.p2 >= 4) and self.p1 != self.p2:
+            s = self.p1N if self.p1 > self.p2 else self.p2N
+            return "Advantage " + s if ((self.p1-self.p2)*(self.p1-self.p2) == 1) else "Win for " + s
 
         if self.p1 == self.p2 == 3:
             return "Deuce"
