233a75a3b7e51ab211f669a4bd3714be3fe2f20d
Vova Gromozdin <74236891+pioppka@users.noreply.github.com>
2023-05-19 12:55:25 +0300

    Update tennis1.cc

--- cpp/tennis1.cc
+++ cpp/tennis1.cc
@@ -1,58 +1,61 @@
 #include <string>
 
 const std::string tennis_score(int p1Score, int p2Score) {
-    std::string score = "";
-    int tempScore=0;
+    std::string score;
     if (p1Score==p2Score)
     {
         switch (p1Score)
         {
             case 0:
-                    score = "Love-All";
+                score = "Love-All";
                 break;
             case 1:
-                    score = "Fifteen-All";
+                score = "Fifteen-All";
                 break;
             case 2:
-                    score = "Thirty-All";
+                score = "Thirty-All";
                 break;
             default:
-                    score = "Deuce";
+                score = "Deuce";
                 break;
 
         }
     }
     else if (p1Score>=4 || p2Score>=4)
     {
-        int minusResult = p1Score-p2Score;
-        if (minusResult==1) score ="Advantage player1";
-        else if (minusResult ==-1) score ="Advantage player2";
-        else if (minusResult>=2) score = "Win for player1";
-        else score ="Win for player2";
+        switch (p1Score-p2Score)
+        {
+                case 1:
+                    score = "Advantage player1";
+                    break;
+                case -1:
+                    score = "Advantage player2";
+                    break;
+                default:
+                    score = (minusResult>=2) ? "Win for player1" : "Win for player2";
+                    break;
+        }
     }
     else
     {
-        for (int i=1; i<3; i++)
+        for (int i=1; i<=2; i++)
         {
-            if (i==1) tempScore = p1Score;
-            else { score+="-"; tempScore = p2Score;}
-            switch(tempScore)
+            switch((i==1) ? p1Score : p2Score)
             {
                 case 0:
-                    score+="Love";
+                    score = "Love";
                     break;
                 case 1:
-                    score+="Fifteen";
+                    score = "Fifteen";
                     break;
                 case 2:
-                    score+="Thirty";
+                    score = "Thirty";
                     break;
                 case 3:
-                    score+="Forty";
+                    score = "Forty";
                     break;
             }
         }
     }
-    return score;
-    
+    return score;   
 }
