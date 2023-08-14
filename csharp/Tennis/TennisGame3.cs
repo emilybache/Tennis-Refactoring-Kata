f196fb3dbb94848ffd0d69bc2b952df744c99bf1
emily <emily@bacheconsulting.com>
2023-08-14 09:12:28 +0200

    revert to the intended starting position

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -2,50 +2,42 @@ namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
-        private int player2Points;
-        private int player1Points;
-        private string player1Name;
-        private string player2Name;
-        public static readonly string[] POINTS_NAMES = new[] { "Love", "Fifteen", "Thirty", "Forty" };
+        private int p2;
+        private int p1;
+        private string p1N;
+        private string p2N;
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            this.p1N = player1Name;
+            this.p2N = player2Name;
         }
 
         public string GetScore()
         {
-            var isNotYetEndgame = (player1Points < 4 && player2Points < 4) && (player1Points + player2Points < 6);
-            if (isNotYetEndgame)
+            string s;
+            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
             {
-                var score = POINTS_NAMES[player1Points];
-                if (player1Points == player2Points)
-                    return score + "-All";
-                else
-                    return score + "-" + POINTS_NAMES[player2Points];
+                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
+                s = p[p1];
+                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
             }
             else
             {
-                if (player1Points == player2Points)
+                if (p1 == p2)
                     return "Deuce";
-                var leader = player1Points > player2Points ? player1Name : player2Name;
-                var pointsDifferenceIsOne = (player1Points - player2Points) * (player1Points - player2Points) == 1;
-                if (pointsDifferenceIsOne)
-                    return "Advantage " + leader;
-                else
-                    return "Win for " + leader;
+                s = p1 > p2 ? p1N : p2N;
+                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
             }
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                this.player1Points += 1;
+                this.p1 += 1;
             else
-                this.player2Points += 1;
+                this.p2 += 1;
         }
 
     }
 }
-
