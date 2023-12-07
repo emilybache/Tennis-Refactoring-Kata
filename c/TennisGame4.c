c68ad2696ba027e2b95049001bd9ee5b74cfa0f7
Peter Kofler <peter.kofler@code-cop.org>
2023-12-07 23:18:59 +0100

    Add missing methods. Still not working.

--- c/TennisGame4.c
+++ c/TennisGame4.c
@@ -5,85 +5,157 @@
 
 #include "TennisGame.h"
 
-struct TennisGame {
+struct TennisGame
+{
     int serverScore;
     int receiverScore;
     const char *server;
     const char *receiver;
-    char result[18];
 };
 
-
-struct TennisGame *TennisGame_Create(const char *player1Name, const char *player2Name) {
+struct TennisGame *TennisGame_Create(const char *player1, const char *player2)
+{
     struct TennisGame *newGame = malloc(sizeof(struct TennisGame));
     newGame->serverScore = 0;
     newGame->receiverScore = 0;
-    newGame->server = player1Name;
-    newGame->receiver = player2Name;
+    newGame->server = player1;
+    newGame->receiver = player2;
     return newGame;
 }
 
-void TennisGame_WonPoint(struct TennisGame *game, const char *playerName) {
-    if (strcmp(playerName, "player1") == 0)
+void TennisGame_WonPoint(struct TennisGame *game, const char *playerName)
+{
+    if (strcmp(game->server, playerName) == 0)
         game->server += 1;
     else
         game->receiver += 1;
 }
 
-
-bool receiverHasAdvantage(struct TennisGame *game) {
+bool receiverHasAdvantage(struct TennisGame *game)
+{
     return game->receiverScore >= 4 && (game->receiverScore - game->serverScore) == 1;
 }
 
-bool serverHasAdvantage(struct TennisGame *game) {
+bool serverHasAdvantage(struct TennisGame *game)
+{
     return game->serverScore >= 4 && (game->serverScore - game->receiverScore) == 1;
 }
 
-bool receiverHasWon(struct TennisGame *game) {
+bool receiverHasWon(struct TennisGame *game)
+{
     return game->receiverScore >= 4 && (game->receiverScore - game->serverScore) >= 2;
 }
 
-bool serverHasWon(struct TennisGame *game) {
+bool serverHasWon(struct TennisGame *game)
+{
     return game->serverScore >= 4 && (game->serverScore - game->receiverScore) >= 2;
 }
 
-bool isDeuce(struct TennisGame *game) {
+bool isDeuce(struct TennisGame *game)
+{
     return game->serverScore >= 3 && game->receiverScore >= 3 && (game->serverScore == game->receiverScore);
 }
 
-struct TennisResult {
-    char* serverScore;
-    char* receiverScore;
+struct TennisResult
+{
+    char serverScore[100];
+    char receiverScore[100];
 };
 
-void formatTennisResult(struct TennisResult *result, char* score) {
+void formatTennisResult(struct TennisResult *result, char *output)
+{
     if (strcmp("", result->receiverScore) == 0)
-        sprintf(score, "%s", result->serverScore);
+        sprintf(output, "%s", result->serverScore);
     else if (strcmp(result->serverScore, result->receiverScore) == 0)
-        sprintf(score, "%s-All", result->serverScore);
+        sprintf(output, "%s-All", result->serverScore);
     else
-        sprintf(score, "%s-%s", result->serverScore, result->receiverScore);
+        sprintf(output, "%s-%s", result->serverScore, result->receiverScore);
 }
 
 typedef struct ResultProvider *ResultProvider_t;
-typedef struct TennisResult (*getResult)(struct ResultProvider resultProvider, struct TennisGame *game);
+typedef void (*getResult)(struct ResultProvider *resultProvider, struct TennisResult *result);
 
-struct ResultProvider {
-    struct TennisGame* game;
-    // TODO: work out how to have a function pointer
-    getResult* functionPtr;
-    ResultProvider_t * nextInChain;
+struct ResultProvider
+{
+    struct TennisGame *game;
+    getResult getResult;
+    ResultProvider_t nextResult;
 };
 
-struct TennisResult* deuce(struct ResultProvider *this, struct TennisGame *game, struct TennisResult *result) {
-    if (isDeuce(game))
-        result->serverScore = "Deuce";
+void deuce_getResult(struct ResultProvider *this, struct TennisResult *result)
+{
+    if (isDeuce(this->game))
+        strcpy(result->serverScore, "Deuce");
     else
-        this->functionPtr(this->nextInChain, game, result);
+        this->getResult(this->nextResult, result);
 }
 
-
-const char *TennisGame_GetScore(struct TennisGame *game) {
-    return "Love-All";
+void gameServer_getResult(struct ResultProvider *this, struct TennisResult *result)
+{
+    if (serverHasWon(this->game))
+    {
+        strcpy(result->serverScore, "Win for ");
+        strcat(result->serverScore, this->game->server);
+    }
+    else
+        this->getResult(this->nextResult, result);
 }
 
+void gameReceiver_getResult(struct ResultProvider *this, struct TennisResult *result)
+{
+    if (receiverHasWon(this->game))
+    {
+        strcpy(result->serverScore, "Win for ");
+        strcat(result->serverScore, this->game->receiver);
+    }
+    else
+        this->getResult(this->nextResult, result);
+}
+
+void advantageServer_getResult(struct ResultProvider *this, struct TennisResult *result)
+{
+    if (serverHasAdvantage(this->game))
+    {
+        strcpy(result->serverScore, "Advantage ");
+        strcat(result->serverScore, this->game->server);
+    }
+    else
+        this->getResult(this->nextResult, result);
+}
+
+void advantageReceiver_getResult(struct ResultProvider *this, struct TennisResult *result)
+{
+    if (receiverHasAdvantage(this->game))
+    {
+        strcpy(result->serverScore, "Advantage ");
+        strcat(result->serverScore, this->game->receiver);
+    }
+    else
+        this->getResult(this->nextResult, result);
+}
+
+void defaultResult_getResult(struct ResultProvider *this, struct TennisResult *result)
+{
+    const char *scores[] = {"Love", "Fifteen", "Thirty", "Forty"};
+    strcpy(result->serverScore, scores[this->game->serverScore]);
+    strcpy(result->receiverScore, scores[this->game->receiverScore]);
+}
+
+const char *TennisGame_GetScore(struct TennisGame *game)
+{
+    char score[200];
+    struct TennisResult result;
+
+    struct ResultProvider resultProvider =
+        {game, deuce_getResult,                 //
+         {game, gameServer_getResult,           //
+          {game, gameReceiver_getResult,        //
+           {game, advantageServer_getResult,    //
+            {game, advantageReceiver_getResult, //
+             {game, defaultResult_getResult, NULL}}}}}};
+
+    resultProvider.getResult(&resultProvider, &result);
+    formatTennisResult(&result, score);
+
+    return score;
+}
