ea5ecfa95e1745bd77b7aa5118aa1025c9f818bb
emily <emily@bacheconsulting.com>
2024-08-16 11:56:24 +0200

    add tennis7 in python

---
+++ python/tennis7.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+class TennisGame7:
+    def __init__(self, player1Name, player2Name):
+        self.player1Name = player1Name
+        self.player2Name = player2Name
+        self.player1Score = 0
+        self.player2Score = 0
+
+    def won_point(self, playerName):
+        if playerName == "player1":
+            self.player1Score += 1
+        else:
+            self.player2Score += 1
+
+    def score(self):
+        result = "Current score: "
+        if self.player1Score == self.player2Score:
+            # tie score
+            result: str
+            match self.player1Score:
+                case 0:
+                    result += "Love-All"
+                case 1:
+                    result += "Fifteen-All"
+                case 2:
+                    result += "Thirty-All"
+                case _:
+                    result += "Deuce"
+
+        elif self.player1Score >= 4 or self.player2Score >= 4:
+            # end-game score
+            if self.player1Score - self.player2Score == 1:
+                result += "Advantage " + self.player1Name
+            elif self.player1Score - self.player2Score == -1:
+                result += "Advantage " + self.player2Name
+            elif self.player1Score - self.player2Score >= 2:
+                result += "Win for " + self.player1Name
+            else:
+                result += "Win for " + self.player2Name
+
+        else:
+            # regular score
+            match self.player1Score:
+                case 0:
+                    result += "Love"
+                case 1:
+                    result += "Fifteen"
+                case 2:
+                    result += "Thirty"
+                case _:
+                    result += "Forty"
+
+            result += "-"
+
+            match self.player2Score:
+                case 0:
+                    result += "Love"
+                case 1:
+                    result += "Fifteen"
+                case 2:
+                    result += "Thirty"
+                case _:
+                    result += "Forty"
+
+        return result + ", enjoy your game!"
--- python/tennis_test.py
+++ python/tennis_test.py
@@ -7,6 +7,7 @@ from tennis3 import TennisGame3
 from tennis4 import TennisGame4
 from tennis5 import TennisGame5
 from tennis6 import TennisGame6
+from tennis7 import TennisGame7
 
 from tennis_unittest import test_cases, play_game
 
@@ -45,3 +46,9 @@ def test_get_score_game5(p1Points, p2Points, score, p1Name, p2Name):
 def test_get_score_game6(p1Points, p2Points, score, p1Name, p2Name):
     game = play_game(TennisGame6, p1Points, p2Points, p1Name, p2Name)
     assert score == game.score()
+
+
+@pytest.mark.parametrize('p1Points p2Points score p1Name p2Name'.split(), test_cases)
+def test_get_score_game7(p1Points, p2Points, score, p1Name, p2Name):
+    game = play_game(TennisGame7, p1Points, p2Points, p1Name, p2Name)
+    assert "Current score: " + score + ", enjoy your game!" == game.score()
